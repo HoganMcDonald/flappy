a7d457f9a0d7daa306d1e49bda44c6a340b35607
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-11-10 14:16:50 -0600

    bird flapping correctly

---
+++ src/v1/scripts/Bird.js
@@ -0,0 +1,41 @@
+class Bird {
+  constructor() {
+    this.x = 190;
+    this.y = height/2;
+    this.size = 30;
+    this.color = 230;
+
+    this.speed = 0;
+    this.acceleration = 0.6;
+    console.log(this.y, this.speed, this.acceleration);
+  }
+
+  flap() {
+    this.speed += -15;
+  }
+
+
+
+
+  render() {
+    fill(this.color);
+    ellipse(this.x, this.y, this.size, this.size);
+  }
+
+  update() {
+    this.speed += this.acceleration;
+    this.speed *= 0.95;
+    this.y += this.speed;
+
+    if (this.y > height) {
+      // bottom
+      this.y = height;
+      this.speed = 0;
+    } else if (this.y + this.speed < 0) {
+      // top
+      this.y = 0;
+      this.speed = 0;
+    }
+
+  } // end update method
+}
--- src/v1/scripts/client.js
+++ src/v1/scripts/client.js
@@ -2,8 +2,17 @@ let bird, pipes;
 
 function setup() {
   createCanvas(1000, 700);
+  bird = new Bird();
 }
 
 function draw() {
   background(20);
+  bird.update();
+  bird.render();
+}
+
+function keyPressed() {
+  if (key = ' ') {
+    bird.flap();
+  }
 }
